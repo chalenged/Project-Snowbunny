6b32dd929d1e43472906801f312d925a098af5dd
Blobi0451 <rantanen0451@gmail.com>
2025-05-20 13:54:41 +0300

    Improved jump + controls and increased gravity
    Jumping is now responsive based on how long you hold down your jump button.
    Added A and D as movement keys in project settings.
    Increased gravity from 980 to 1300 in project settings.

--- Scripts/blob_player.gd
+++ Scripts/blob_player.gd
@@ -6,23 +6,39 @@ const JUMP_VELOCITY = -400.0
 const MAX_JUMPS = 2
 var jumps: int = 0
 var grounded: bool = false
+var jumpRelease: bool = true
+var gravity
 
 
 func _physics_process(delta: float) -> void:
+	# Get gravity at the beginning of every frame
+	gravity = get_gravity()
 	# Check if player is on floor and resets jumps to max and adds gravity if not.
 	if is_on_floor():
 		grounded = true
 		jumps = MAX_JUMPS
 	else:
 		grounded = false
-		velocity += get_gravity() * delta
+		# Jump higher if jump button is held down and fall faster if jump is released
+		if !jumpRelease and velocity.y < 0:
+			velocity += gravity * delta * 0.8
+		elif jumpRelease and velocity.y < 100:
+			velocity += gravity * delta * 2
+		else:
+			velocity += gravity * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and jumps>0:
 		if grounded:
 			velocity.y = JUMP_VELOCITY
+			jumpRelease = false
 		else:
 			velocity.y = JUMP_VELOCITY*0.85
+			jumpRelease = false
+	
+	# Check if spacebar is not held
+	if Input.is_action_just_released("ui_accept") and !jumpRelease:
+		jumpRelease = true;
 		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
