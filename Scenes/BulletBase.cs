7ffd8d1ac95c702a4362fb5296dad290798ceb92
chalenged <1337.chalenged@gmail.com>
2025-06-13 09:18:41 -0700

    Fixed funny area2d issue with bullets teehee

--- Scenes/BulletBase.cs
+++ Scenes/BulletBase.cs
@@ -4,7 +4,7 @@ using System;
 public partial class BulletBase : RigidBody2D
 {
 	public float speed = 1000.0f;
-	public override void _Process(double _delta)
+	public override void _PhysicsProcess(double _delta)
 	{
 		Rotation = LinearVelocity.Angle();
 		LinearVelocity = LinearVelocity.Normalized() * (speed * GameController.Instance.timeScale) ;
