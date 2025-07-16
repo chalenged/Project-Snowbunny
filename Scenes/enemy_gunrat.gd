76f56ccc4a9d6937ead9a3fd3783f4c02ed9a96f
michellellelle <94235856+michellellelle@users.noreply.github.com>
2025-07-16 18:16:41 +0100

    new enemy
    added gunrat, the rat with a gun
    run m_gunscene to test it
    known bugs: gunrat damages themself when they shoot lol

--- Scenes/enemy_gunrat.gd
+++ Scenes/enemy_gunrat.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-
+#gunrat, the rat with a gun
 const SPEED = 100.0
 var gravity: Vector2
 var current_state
@@ -9,23 +9,22 @@ var playerDistanceY
 var direction
 var Health = 5
 var attackCooldown = 0
+var bullet = preload("res://Scenes/BulletBase.tscn")
 
-#runs when scene is loaded for the first time
 func _ready():
-	#disable attack hurtbox and hide attack sprite
-	$TestAttackHurtbox.set_collision_mask_value(3, false)
 	$AnimatedSprite2D.play("idle")
-	
-func _physics_process(delta): 
+
+func _physics_process(delta):
 	attackCooldown += delta * $"/root/GameController".timeScale
 	#get distance to player
 	playerDistance = $"/root/GameController".playerPos.distance_to(position)
+	direction = ($"/root/GameController".playerPos - position).normalized()
 	gravity = get_gravity()
 	if is_on_floor() == false:
 		velocity.y += gravity.y * delta
 	
 	if Health < 1:
-		death()
+		_death()
 	#get direction to player
 	direction = ($"/root/GameController".playerPos - position).normalized()
 	
@@ -36,63 +35,61 @@ func _physics_process(delta):
 		rotation_degrees = 0
 		scale.y = 1
 	
-	#check if player is between 300 and 50 pixels, moves towards player if they are
-	if $AnimatedSprite2D.get_animation() != "death" && $AnimatedSprite2D/AnimationPlayer.get_current_animation() != "attackAnimation":
-		if (playerDistance <= 300 and playerDistance > 35):
-			current_state = 1
-		#attack player if they are within 50 pixels
-		elif playerDistance <= 35 && attackCooldown >= 2:
+	if $AnimatedSprite2D.get_animation() != "death" && $AnimatedSprite2D.get_animation() != "hit":
+		if playerDistance < 600 && playerDistance >= 300 && attackCooldown >= 5:
 			current_state = 2
-	
-		#idle state if player is too far away
+		elif playerDistance < 300:
+			current_state = 1
 		else:
 			current_state = 0
 		match(current_state):
 			0:
 				_idle_state()
 			1:
-				_angry_state()
+				_run_state()
 			2:
 				_attack()
 	
 	move_and_slide()
 	
 func _idle_state():
-	velocity.x = velocity.x * 0
-	if $AnimatedSprite2D.is_playing() == false:
-		$AnimatedSprite2D.play("idle")
-	return
+	velocity.x = 0
+	$AnimatedSprite2D.play("idle")
 	
-func _angry_state():
+func _run_state():
 	playerDistanceY = $"/root/GameController".playerPos.y - position.y
-	direction = ($"/root/GameController".playerPos - position).normalized() 
 	if $AnimatedSprite2D.get_animation() != "hit":
 		$AnimatedSprite2D.play("idle")
-	velocity.x = direction.x * SPEED * $"/root/GameController".timeScale
+	velocity.x = -direction.x * SPEED * $"/root/GameController".timeScale
 	return
-	
+
 func _attack():
 	velocity.x = 0
-	$AnimatedSprite2D.play("attack")
-	$AnimatedSprite2D/AnimationPlayer.play("attackAnimation")
+	$AnimatedSprite2D.play("idle")
 	attackCooldown = 0
+	var instance = bullet.instantiate()
+	instance.set_collision_layer_value(2,false)
+	instance.set_collision_layer_value(3, true)
+	instance.set_linear_velocity(direction)
+	add_child(instance)
 	return
-
-
-func _on_bullet_collision_area_entered(_area):
-	velocity.x = 0
-	Health = Health - 1
-	print(Health)
-	if $AnimatedSprite2D/AnimationPlayer.get_current_animation() != "attackAnimation":
-		$AnimatedSprite2D.frame = 0
-		$AnimatedSprite2D.play ("hit")
-	return 
 	
-func death():
+func _death():
 	$AnimatedSprite2D.play("death")
 	velocity.x = randf_range(50,100) * -direction.x
 	set_collision_layer_value(1, false)
 	$BulletCollision.set_collision_mask_value(2, false)
 	await get_tree().create_timer(1.0).timeout
 	queue_free()
+	return
+	
+
+
+func _on_bullet_collision_area_entered(area):
+	velocity.x = 0
+	Health = Health - 1
+	$AnimatedSprite2D.frame = 0
+	$AnimatedSprite2D.play("hit")
+	await $AnimatedSprite2D.animation_finished
+	$AnimatedSprite2D.play("idle")
 	return
